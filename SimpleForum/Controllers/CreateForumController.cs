ab4a7fefffc9b15cb297a7f2bb7b7aad3ed6e27a
Jakub Cink <jakub.blik.cink@gmail.com>
2022-06-10 20:47:52 +0200

    Added CreateForumController

---
+++ SimpleForum/Controllers/CreateForumController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleForum.Data;
+using SimpleForum.Models;
+
+namespace SimpleForum.Controllers
+{
+    public class CreateForumController : Controller
+    {
+        private ApplicationDbContext DbContext;
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateForum(Forum forum)
+        {
+            DbContext.Forums.Add(forum);
+            DbContext.SaveChanges();
+
+            return View();
+        }
+    }    
+}
